9c994f9b4556e576298a680396ced6b820a7d776
Joel Gilley <gilleyj@gmail.com>
2016-11-24 00:04:13 +0100

    - expanded entrypoint for service

--- 3.4/container_confs/entrypoint.sh
+++ 3.4/container_confs/entrypoint.sh
@@ -5,6 +5,7 @@
 set -e
 
 HOST_IP=`/bin/grep $HOSTNAME /etc/hosts | /usr/bin/cut -f1`
+export HOST_IP=${HOST_IP}
 echo
 echo "container started with ip: ${HOST_IP}..."
 echo
@@ -19,10 +20,13 @@ done
 if [ "$1" == "supervisor" ]; then
 	echo "starting supervisord...."
 	/usr/bin/supervisord -n -c /etc/supervisord.conf
+elif [ "$1" == "service" ]; then
+	echo "starting /service.sh..."
+	exec /service.sh
 elif [ "$1" == "bash" ] || [ "$1" == "shell" ]; then
 	echo "starting /bin/bash with /etc/profile..."
 	/bin/bash --rcfile /etc/profile
 else
-	echo "Running something else"
+	echo "Running something else ($@)"
 	exec "$@"
 fi
